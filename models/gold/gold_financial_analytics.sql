3cf6f1b4998f2ddf6010a521f7755531b8936da4
lunita-moon <lunitamoon110@gmail.com>
2026-04-14 19:59:52 +0200

    Create gold_financial_analytics.sql

---
+++ models/gold/gold_financial_analytics.sql
@@ -0,0 +1,21 @@
+-- gold_financial_analytics.sql
+{{ config(materialized='table') }}
+
+SELECT
+    -- Dimensiones
+    dt.year,
+    dt.month,
+    c.clinic_name,
+    doc.first_name || ' ' || doc.last_name as doctor_name,
+    -- Métricas Financieras
+    b.billing_id,
+    b.total_amount,
+    b.insurance_amount,
+    b.copay_amount,
+    b.payment_method,
+    -- Flag para facilitar proporción en PySpark
+    CASE WHEN b.insurance_amount > 0 THEN 'Insurance' ELSE 'Direct' END as revenue_source
+FROM {{ ref('fact_billing') }} b
+LEFT JOIN {{ ref('dim_clinics') }} c ON b.clinic_id = c.clinic_id
+LEFT JOIN {{ ref('dim_doctor') }} doc ON b.doctor_id = doc.doctor_id
+LEFT JOIN {{ ref('dim_date') }} dt ON b.date_id = dt.date_id
