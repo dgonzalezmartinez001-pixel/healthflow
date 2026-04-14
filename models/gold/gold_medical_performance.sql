6f68661c5efd77473e194c5c29968ab80bacfd23
lunita-moon <lunitamoon110@gmail.com>
2026-04-14 19:59:01 +0200

    Create gold_medical_performance.sql

---
+++ models/gold/gold_medical_performance.sql
@@ -0,0 +1,20 @@
+-- gold_medical_performance.sql
+{{ config(materialized='table') }}
+
+SELECT
+    -- Dimensiones temporales
+    d.date_id,
+    d.year,
+    d.month,
+    -- Dimensiones de Clínica
+    a.clinic_name,
+    a.clinic_city,
+    -- Dimensiones de Doctor
+    a.doctor_name,
+    a.doctor_specialty,
+    -- Métricas
+    a.appointment_id,
+    a.status, -- Para filtrar por 'Completed'
+    a.appointment_type
+FROM {{ ref('dim_appointments') }} a
+LEFT JOIN {{ ref('dim_date') }} d ON a.date_id = d.date_id
